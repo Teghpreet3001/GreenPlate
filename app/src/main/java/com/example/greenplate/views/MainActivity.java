3cbce31e030ae7d9535bf9001143d80d1f51836f
shreyshd2004 <145157669+shreyshd2004@users.noreply.github.com>
2024-02-22 16:20:30 -0500

    Signup page centered & SignUpActivity class added
    * signup page
    
    * fixed align
    
    * Centered Sign Up page and added SignUpActivity class
    
    * remove idea director + increase sign up page width + add idea in gitignore
    
    ---------
    
    Co-authored-by: Rachit Gupta <guptarachit2004@gmail.com>

--- app/src/main/java/com/example/greenplate/views/MainActivity.java
+++ app/src/main/java/com/example/greenplate/views/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.greenplate.views;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
 import com.example.greenplate.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create an Intent to start SignUpActivity
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+
+        // Optional: If you don't want users to return to MainActivity by pressing back,
+        // call finish() after starting the activity
+        finish();
     }
 }
