74ddd725aa662f0a98980a0f5f5268bec465f420
Rachit Gupta <guptarachit2004@gmail.com>
2024-02-25 21:39:53 -0500

    Adds User model to the model directory

--- app/src/main/java/com/example/greenplate/views/SignUpActivity.java
+++ app/src/main/java/com/example/greenplate/views/SignUpActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.greenplate.R;
-import com.example.greenplate.User;
+import com.example.greenplate.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
