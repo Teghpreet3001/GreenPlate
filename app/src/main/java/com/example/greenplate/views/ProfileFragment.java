d4f067e6cf31c5931531a9503de5ecd3f8f9a03f
Rachit Gupta <guptarachit2004@gmail.com>
2024-03-09 16:17:55 -0500

    Add Profile Page Skeleton

---
+++ app/src/main/java/com/example/greenplate/views/ProfileFragment.java
@@ -0,0 +1,57 @@
+package com.example.greenplate.views;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.greenplate.viewmodels.ProfileViewModel;
+import com.example.greenplate.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileFragment extends Fragment {
+
+    private ProfileViewModel mViewModel;
+
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button signOutBtn = v.findViewById(R.id.signOutBtn);
+
+        signOutBtn.setOnClickListener(v_1 -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            startActivity(intent);
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+
+
+
+
+
+}
