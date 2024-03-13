25e7fb0e2159655661f7606c6040b2bea9314ea4
Teghpreet3001 <teg3001@gmail.com>
2024-03-12 22:33:36 -0400

    rebases with main

--- app/src/test/java/com/example/greenplate/userUnitTest.java
+++ app/src/test/java/com/example/greenplate/userUnitTest.java
@@ -10,6 +10,7 @@ import com.example.greenplate.models.User;
 
 
 public class userUnitTest {
+    // Aditya's Tests
     @Test
         public void testAgeUpdate() {
         User user = new User("Aditya", "Kabu", "kabuaditya@example.com", "80", "180", "Male", "20");
@@ -22,5 +23,22 @@ public class userUnitTest {
         User user = new User("Aditya", "Kabu", "kabuaditya@example.com", "80", "180", "Female", "20");
         user.setGender("Male");
         assertEquals("Gender should now be Male", "Male", user.getGender());
+    }
+
+    // Teghpreet's Tests
+    @Test
+    public void negativeAge() {
+        User user = new User("Teghpreet", "Singh", "teg3001@gamil.com", "180", "70", "Male", "21");
+        user.setAge("-21");
+        String age = user.getAge();
+        assertEquals("Age will not change","21",age);
+    }
+
+    @Test
+    public void invalidGender() {
+        User user = new User("Teghpreet", "Singh", "teg3001@gamil.com", "180", "70", "Male", "21");
+        user.setGender("not a valid gender");
+        String gender = user.getGender();
+        assertEquals("Gender will not change", "Male", gender);
     }
 }
